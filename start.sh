258b48cf3e25561a0bea542ba420c0708a9f8a97
WOODcraft <121882568+SudoR2spr@users.noreply.github.com>
2023-12-11 03:07:08 +0530

    Opleech-Filter-Bot 🦋

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/SudoR2spr/Opleech-Filter-Bot.git /filterbot 
+  git clone https://github.com/SudoR2spr/Opleech-Filter-Bot.git /Opleech-Filter-Bot 
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /filterbot
+  git clone $UPSTREAM_REPO /Opleech-Filter-Bot
 fi
-cd /filterbot
+cd /Opleech-Filter-Bot
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
