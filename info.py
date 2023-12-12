73597f9ab87cbcab067ff56e79370183f4ddd94e
WOODcraft <121882568+SudoR2spr@users.noreply.github.com>
2023-12-12 21:56:49 +0530

    Opleech-Filter-Bot 🦋

--- info.py
+++ info.py
@@ -15,7 +15,7 @@ def is_enabled(value, default):
 SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ.get('API_ID', '24314942'))
 API_HASH = environ.get('API_HASH', '608c07ba57d9130d7974c9b3639ec570')
-BOT_TOKEN = environ.get('BOT_TOKEN', "6736774847:AgH2SgQfyFhxV6BS-I1PkRyXLXl5FBEPaN4")
+BOT_TOKEN = environ.get('BOT_TOKEN', "6736774847:AgH2SgQ5yFhxV6BS-I1PkRyXLXl5FBEPaN4")
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
@@ -75,7 +75,7 @@ LONG_IMDB_DESCRIPTION = is_enabled(environ.get("LONG_IMDB_DESCRIPTION", "False")
 SPELL_CHECK_REPLY = is_enabled(environ.get("SPELL_CHECK_REPLY", "True"), True)
 MAX_LIST_ELM = environ.get("MAX_LIST_ELM", None)
 INDEX_REQ_CHANNEL = int(environ.get('INDEX_REQ_CHANNEL', LOG_CHANNEL))
-FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '-1001571381165')).split()]
+FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '-1001571381145')).split()]
 MELCOW_NEW_USERS = is_enabled((environ.get('MELCOW_NEW_USERS', "True")), True)
 PROTECT_CONTENT = is_enabled((environ.get('PROTECT_CONTENT', "False")), False)
 PUBLIC_FILE_STORE = is_enabled((environ.get('PUBLIC_FILE_STORE', "True")), True)
